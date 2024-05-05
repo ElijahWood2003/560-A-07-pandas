6915956c920579be5b981eca574770eaea602698
ElijahWood2003 <elijahdwood@gmail.com>
2024-05-05 15:41:37 -0400

    10 Athletes df

--- roster.py
+++ roster.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Link to roster: https://goheels.com/sports/mens-basketball/roster
 
 # Step 1: Create list of 3 last names and print
-roster = ["Bacot", "Davis", "Cadeau"]
+# roster = ["Bacot", "Davis", "Cadeau"]
 # print(f"{roster}")
 
 # Step 2: Print list using a for loop
@@ -15,6 +15,20 @@ roster = ["Bacot", "Davis", "Cadeau"]
 # print(data)
 
 # Step 4: Make last name dataframe
-player = {"Last Name": roster}
+# player = {"Last Name": roster}
+# data = pd.DataFrame(player)
+# print(data)
+
+# Step 5: 10 athletes in the dataframe
+player = {"Last Name": ["Bacot", "Davis", "Cadeau", "High", "Ryan", "Trimble", "Wojcik", "Washington", "Lebo", "Landry"],
+            "First Name": ["Armando", "RJ", "Elliot", "Zayden", "Cormac", "Seth", "Paxson", "Jalen", "Creighton", "Rob"],
+            "height": [83, 72, 73, 80, 74, 75, 78, 74, 72, 70],
+            "weight": [240, 180, 180, 200, 210, 185, 184, 205, 190, 215]
+            }
 data = pd.DataFrame(player)
+
+data["bmi"] = ((data["weight"]/2.025) / ((data["height"]/39.37)**2)).round(2)
+
 print(data)
+
+data.to_csv("bmi.csv")
