4625a1af06e451a9417dfd562f0bd99a5e78a487
ElijahWood2003 <elijahdwood@gmail.com>
2024-05-05 15:31:18 -0400

    roster list

--- roster.py
+++ roster.py
@@ -1,14 +1,7 @@
 import pandas as pd
 
-player = {"Last Name": ["Bacot", "Davis", "Cadeau", "High", "Ryan", "Trimble", "Wojcik", "Washington", "Lebo", "Landry"],
-            "First Name": ["Armando", "RJ", "Elliot", "Zayden", "Cormac", "Seth", "Paxson", "Jalen", "Creighton", "Rob"],
-            "height": [83, 72, 73, 80, 74, 75, 78, 74, 72, 70],
-            "weight": [240, 180, 180, 200, 210, 185, 184, 205, 190, 215]
-            }
-data = pd.DataFrame(player)
+# Link to roster: https://goheels.com/sports/mens-basketball/roster
 
-data["bmi"] = ((data["weight"]/2.025) / ((data["height"]/39.37)**2)).round(2)
-
-print(data)
-
-data.to_csv("bmi.csv")
+# Step 1: Create list of 3 last names and print
+roster = {"Bacot", "Davis", "Cadeau"}
+print(f"{roster}")
