e53f27a8dceef4489e880cf0be6fe858e97fe689
ElijahWood2003 <elijahdwood@gmail.com>
2024-05-05 15:39:46 -0400

    Last Name Header

--- roster.py
+++ roster.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Link to roster: https://goheels.com/sports/mens-basketball/roster
 
 # Step 1: Create list of 3 last names and print
-roster = {"Bacot", "Davis", "Cadeau"}
+roster = ["Bacot", "Davis", "Cadeau"]
 # print(f"{roster}")
 
 # Step 2: Print list using a for loop
@@ -11,5 +11,10 @@ roster = {"Bacot", "Davis", "Cadeau"}
 #     print(f"{name}")
 
 # Step 3: Make dataframe and print
-data = pd.DataFrame(roster)
+# data = pd.DataFrame(roster)
+# print(data)
+
+# Step 4: Make last name dataframe
+player = {"Last Name": roster}
+data = pd.DataFrame(player)
 print(data)
